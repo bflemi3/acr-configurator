acffeb19e050ec5af260833635c49d86b90eca0b
Jay Lockwood <jay@lockwoodsoftware.com>
2017-05-13 18:33:40 -0400

    - Removed http from hostname - it appeared to be causing a connect problem - added a semi-colon to end of SQL to ACR; ACR needs a trailing semi-colon - jsBeautify by default (sorry)

--- database/clients/AqlClient.js
+++ database/clients/AqlClient.js
@@ -1,40 +1,44 @@
-const AbstractDatabaseClient = require('./AbstractDatabaseClient'),
-    Promise = require('bluebird'),
-    config = require('../../config.json').queries,
-    _ = require('lodash'),
-    aql = require('net').Socket(),
-    notImplemented = require('../../util/notImplemented');
+const AbstractDatabaseClient = require( './AbstractDatabaseClient' ),
+    Promise = require( 'bluebird' ),
+    config = require( '../../config.json' ).queries,
+    _ = require( 'lodash' ),
+    aql = require( 'net' ).Socket(),
+    notImplemented = require( '../../util/notImplemented' );
 
 const queryParts = {
-    select(values) { return `select ${values}`; },
-    from(value) { return `from ${value}`; },
-    where(clientItems, config) {
-        if(!_.isPlainObject(clientItems))
-            throw new TypeError(`Unable to build SQL statement. Invalid where object given.`);
+    select( values ) {
+        return `select ${values}`;
+    },
+    from( value ) {
+        return `from ${value}`;
+    },
+    where( clientItems, config ) {
+        if ( !_.isPlainObject( clientItems ) )
+            throw new TypeError( `Unable to build SQL statement. Invalid where object given.` );
 
-        if(!Array.isArray(config.where))
-            throw new TypeError(`Unable to build SQL statement. Invalid configuration where object. The where property of the configuration must be an Array.`);
+        if ( !Array.isArray( config.where ) )
+            throw new TypeError( `Unable to build SQL statement. Invalid configuration where object. The where property of the configuration must be an Array.` );
 
-        function clause(field, operator = '=', value) {
+        function clause( field, operator = '=', value ) {
             return `${field} ${operator} ${value}`;
         }
 
-        function resolve(clientItems, operator = whereOperators.and) {
+        function resolve( clientItems, operator = whereOperators.and ) {
             let clauses = [],
                 sql = '';
 
-            for(let [key, value] of Object.entries(clientItems)) {
-                if(whereOperators[key]) {
-                    clauses.push(sql += `${resolve(value, whereOperators[key])} `);
+            for ( let [ key, value ] of Object.entries( clientItems ) ) {
+                if ( whereOperators[ key ] ) {
+                    clauses.push( sql += `${resolve(value, whereOperators[key])} ` );
                     continue;
                 }
 
-                const item = config.where.find(w => w.name === key);
-                if(!item) continue;
-                clauses.push(clause(item.field, value.operator, value.value));
+                const item = config.where.find( w => w.name === key );
+                if ( !item ) continue;
+                clauses.push( clause( item.field, value.operator, value.value ) );
             }
 
-            sql += operator(clauses);
+            sql += operator( clauses );
             return sql;
         }
 
@@ -43,31 +47,31 @@ const queryParts = {
 };
 
 const whereOperators = {
-    and(clauses) {
+    and( clauses ) {
         return !clauses.length ? '' : `(${clauses.join(' AND ')})`;
     },
-    or(clauses) {
+    or( clauses ) {
         return !clauses.length ? '' : `(${clauses.join(' OR ')})`;
     }
 };
 
-function buildSql(query, config) {
-    return Object.entries(query)
-        .map(([key, value]) => {
+function buildSql( query, config ) {
+    return Object.entries( query )
+        .map( ( [ key, value ] ) => {
             key = key.toLowerCase();
-            if(!_.isFunction(queryParts[key])) return;
-            return queryParts[key](value, config);
-        })
-        .filter(p => p)
-        .join(' ');
+            if ( !_.isFunction( queryParts[ key ] ) ) return;
+            return queryParts[ key ]( value, config );
+        } )
+        .filter( p => p )
+        .join( ' ' ) + ';';
 }
 
 module.exports = class AqlClient extends AbstractDatabaseClient {
-    constructor(config) {
-        super(config);
+    constructor( config ) {
+        super( config );
 
-        if(!config.port)
-            throw new Error(`Unable to construct instance of ${this.constructor.name}, missing 'port' configuration value.`);
+        if ( !config.port )
+            throw new Error( `Unable to construct instance of ${this.constructor.name}, missing 'port' configuration value.` );
 
         this.port = config.port;
     }
@@ -76,12 +80,12 @@ module.exports = class AqlClient extends AbstractDatabaseClient {
      * Connect to the aql database
      */
     connect() {
-        return new Promise((resolve, reject) => {
-            aql.connect(this.port, this.host, () => {
+        return new Promise( ( resolve, reject ) => {
+            aql.connect( this.port, this.host, () => {
                 resolve();
-            });
-            aql.on('error', error => reject(error));
-        });
+            } );
+            aql.on( 'error', error => reject( error ) );
+        } );
     }
 
     /**
@@ -89,30 +93,34 @@ module.exports = class AqlClient extends AbstractDatabaseClient {
      * @param query {Object}
      * @returns {Promise<*>}
      */
-    select(query) {
-        return new Promise((resolve, reject) => {
-            if(!_.isPlainObject(query)) query = { select: query };
+    select( query ) {
+        return new Promise( ( resolve, reject ) => {
+            if ( !_.isPlainObject( query ) ) query = {
+                select: query
+            };
 
-            if(!query.select)
-                return reject(new TypeError(`Invalid query object. 'select' must be specified in the query argument`));
+            if ( !query.select )
+                return reject( new TypeError( `Invalid query object. 'select' must be specified in the query argument` ) );
 
-            if(!query.from) query.from = config.get.from;
+            if ( !query.from ) query.from = config.get.from;
 
-            this.connect(() => {
-                aql.write(buildSql(query, config.get), (error, data) => {
+            console.log( buildSql( query, config.get ) );
+
+            this.connect( () => {
+                aql.write( buildSql( query, config.get ), ( error, data ) => {
                     aql.destroy();
-                    if(error) return reject(error);
-                    resolve(JSON.stringify(data));
-                });
-            });
-        });
+                    if ( error ) return reject( error );
+                    resolve( JSON.stringify( data ) );
+                } );
+            } );
+        } );
     }
 
     /**
      * Update the given configuration object in the aql database
      * @param query
      */
-    update(query) {
+    update( query ) {
         return notImplemented();
     }
 };
