8bccec9c1d47bf6a282e98675f27ee348cf3f66b
Brandon <brand.fleming@gmail.com>
2017-06-01 21:19:08 -0400

    Added semicolon to end generated sql string

--- sqlBuilder/builder.js
+++ sqlBuilder/builder.js
@@ -4,6 +4,6 @@ module.exports = function builder(builders, table, sql) {
     if(!Array.isArray(builders)) builders = [builders];
 
     return assign({
-        toString() { return sql; }
+        toString() { return `${sql};`; }
     }, builders, table, sql);
 };
