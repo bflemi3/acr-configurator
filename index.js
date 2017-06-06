c2b7620f685d8d0c02c718dbfe2ce4d9c1e2347c
Jay Lockwood <jay@lockwoodsoftware.com>
2017-06-05 21:01:30 -0400

    false error message!

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const app = require('express')(),
-    bodyParser = require('body-parser'),
+const app = require( 'express' )(),
+    bodyParser = require( 'body-parser' ),
     port = process.env.PORT || 8081,
-    url = require('url'),
-    cors = require('cors'),
-    Promise = require('bluebird'),
-    _ = require('lodash'),
-    config = require('./config.json'),
-    databaseClientProvider = require('./database/databaseClientProvider'),
-    translate = require('object-translation'),
+    url = require( 'url' ),
+    cors = require( 'cors' ),
+    Promise = require( 'bluebird' ),
+    _ = require( 'lodash' ),
+    config = require( './config.json' ),
+    databaseClientProvider = require( './database/databaseClientProvider' ),
+    translate = require( 'object-translation' ),
     translations = config.translations,
-    configurations = new (require('./database/repositories/CustomerConfigurationsRepository'))(databaseClientProvider(config.database));
+    configurations = new( require( './database/repositories/CustomerConfigurationsRepository' ) )( databaseClientProvider( config.database ) );
 
 /**
  * Handles logging errors and responding to the client with error messages
@@ -17,95 +17,101 @@ const app = require('express')(),
  * @param response
  * @param status
  */
-function errorHandler(error, response, status = 500) {
+function errorHandler( error, response, status = 500 ) {
     const message = error instanceof Error ? error.message : error;
 
-    status = _.isNumber(response) ? response : status;
-    response = _.isFunction(response.send) ? response : undefined;
+    status = _.isNumber( response ) ? response : status;
+    response = _.isFunction( response.send ) ? response : undefined;
 
-    console.error(error.stack || message);
+    console.error( error.stack || message );
 
-    if(response)
-        response.status(status).send(message);
+    if ( response )
+        response.status( status ).send( message );
 }
 
 // setup middleware
-app.use(cors());
-app.use(bodyParser.json());
+app.use( cors() );
+app.use( bodyParser.json() );
 
 // setup error handlers for uncaught rejections and errors
-process.on('uncaughtException', error => console.error(`Uncaught exception... ${error.stack}`));
-process.on('unhandledRejection', error => {
-    console.error(`Uncaught rejection... ${error.stack}`);
-    process.exit(1);
-});
+process.on( 'uncaughtException', error => console.error( `Uncaught exception... ${error.stack}` ) );
+process.on( 'unhandledRejection', error => {
+    console.error( `Uncaught rejection... ${error.stack}` );
+    process.exit( 1 );
+} );
 
 /**
  * GET /configuration
  * Get all configuration objects from the database
  */
-app.get('/configuration', Promise.coroutine(function*(request, response) {
-    console.log(`GET /configuration`);
+app.get( '/configuration', Promise.coroutine( function*( request, response ) {
+    console.log( `GET /configuration` );
     try {
         const result = yield configurations.get();
-        if(!result || !result.length) return errorHandler(`There was an issue retrieving results.`, response);
 
-        response.json(translate(translations.serverToClient, result));
-    } catch(error) {
-        errorHandler(error, response);
+        // @TODO - this is a problem!
+        // if ( !result || !result.length ) return errorHandler( `There was an issue retrieving results.`, response );
+
+        response.json( translate( translations.serverToClient, result ) );
+    } catch ( error ) {
+        errorHandler( error, response );
     }
-}));
+} ) );
 
 /**
  * GET /configuration/:serialNumber
  * Get one configuration object given the serial number
  * @params serialNumber
  */
-app.get('/configuration/:serialNumber', Promise.coroutine(function*(request, response) {
-    console.log(`GET /configuration/${request.params.serialNumber}`);
+app.get( '/configuration/:serialNumber', Promise.coroutine( function*( request, response ) {
+    console.log( `GET /configuration/${request.params.serialNumber}` );
     try {
-        const result = yield configurations.get({ serialNumber: request.params.serialNumber });
-        if(!result) return errorHandler('Configuration object not found.', response, 404);
+        const result = yield configurations.get( {
+            serialNumber: request.params.serialNumber
+        } );
+        if ( !result ) return errorHandler( 'Configuration object not found.', response, 404 );
 
-        response.json(translate(translations.serverToClient, result));
-    } catch(error) {
-        errorHandler(error, response);
+        response.json( translate( translations.serverToClient, result ) );
+    } catch ( error ) {
+        errorHandler( error, response );
     }
-}));
+} ) );
 
 /**
  * PUT /configuration/:serialNumber
  * Update one configuration object given the serial number
  * @param serialNumber
  */
-app.put('/configuration/:serialNumber', Promise.coroutine(function*(request, response) {
-    console.log(`PUT /configuration/${request.params.serialNumber}`);
+app.put( '/configuration/:serialNumber', Promise.coroutine( function*( request, response ) {
+    console.log( `PUT /configuration/${request.params.serialNumber}` );
     // @todo: This has not been implemented in the AqlClient
     try {
 
-        const result = yield configurations.update(translate(translations.clientToServer, request.body), { serialNumber: request.params.serialNumber });
-        if(!result) return errorHandler(`There was an issue updating the configuration object for serial number '${request.params.serialNumber}'.`, response);
-        response.json(result);
-    } catch(error) {
-        errorHandler(error, response);
+        const result = yield configurations.update( translate( translations.clientToServer, request.body ), {
+            serialNumber: request.params.serialNumber
+        } );
+        if ( !result ) return errorHandler( `There was an issue updating the configuration object for serial number '${request.params.serialNumber}'.`, response );
+        response.json( result );
+    } catch ( error ) {
+        errorHandler( error, response );
     }
-}));
+} ) );
 
 /**
  * POST /configuration
  * Create a new configuration object
  */
-app.post('/configuration', Promise.coroutine(function*(request, response) {
+app.post( '/configuration', Promise.coroutine( function*( request, response ) {
     try {
-        const result = yield configurations.insert(translate(translations.clientToServer, request.body));
-        if(!result) return errorHandler(`There was an issue creating the configuration object.`, response);
-        response.json(result);
-    } catch(error) {
-        errorHandler(error, response);
+        const result = yield configurations.insert( translate( translations.clientToServer, request.body ) );
+        if ( !result ) return errorHandler( `There was an issue creating the configuration object.`, response );
+        response.json( result );
+    } catch ( error ) {
+        errorHandler( error, response );
     }
-}));
+} ) );
 
 // startup the server
-app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
-});
+app.listen( port, () => {
+    console.log( `Listening on port ${port}` );
+} );
