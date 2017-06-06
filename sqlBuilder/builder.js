ee0d10dd8b96c5bac4195c5e41605f406807e38d
bfleming <brandon.fleming@lmco.com>
2017-06-06 08:09:34 -0400

    Fixing Jay's crappy formatting

--- sqlBuilder/builder.js
+++ sqlBuilder/builder.js
@@ -1,14 +1,9 @@
 const assign = require( './util/assign' );
 
 module.exports = function builder( builders, table, sql ) {
-
 	if ( !Array.isArray( builders ) ) builders = [ builders ];
 
-	console.log( `${sql};` );
-
 	return assign( {
-		toString() {
-			return `${sql};`;
-		}
+		toString() { return `${sql};`; }
 	}, builders, table, sql );
 };
