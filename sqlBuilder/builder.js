f5ea343859bbe8235c7d0bf939e451278e31f972
Jay Lockwood <jay@lockwoodsoftware.com>
2017-06-05 20:21:09 -0400

    reformat (sorry) and add a space after the comma trailing table names

--- sqlBuilder/builder.js
+++ sqlBuilder/builder.js
@@ -1,9 +1,14 @@
-const assign = require('./util/assign');
+const assign = require( './util/assign' );
 
-module.exports = function builder(builders, table, sql) {
-    if(!Array.isArray(builders)) builders = [builders];
+module.exports = function builder( builders, table, sql ) {
 
-    return assign({
-        toString() { return `${sql};`; }
-    }, builders, table, sql);
+	if ( !Array.isArray( builders ) ) builders = [ builders ];
+
+	console.log( `${sql};` );
+
+	return assign( {
+		toString() {
+			return `${sql};`;
+		}
+	}, builders, table, sql );
 };
--- sqlBuilder/select.js
+++ sqlBuilder/select.js
@@ -1,16 +1,16 @@
-const builders = [require('./join'), require('./where'), require('./orderBy')],
-    builder = require('./builder');
+const builders = [ require( './join' ), require( './where' ), require( './orderBy' ) ],
+    builder = require( './builder' );
 
-function select(table) {
+function select( table ) {
 
     return {
-        select(fields) {
-            if(!Array.isArray(fields)) fields = [fields];
-            if(fields.includes(select.STAR) && fields.length > 1)
-                throw new Error(`Invalid number of select fields. '${select.STAR}' can be the only field in the select.`);
+        select( fields ) {
+            if ( !Array.isArray( fields ) ) fields = [ fields ];
+            if ( fields.includes( select.STAR ) && fields.length > 1 )
+                throw new Error( `Invalid number of select fields. '${select.STAR}' can be the only field in the select.` );
 
-            const sql = `select ${table.getFields(fields, select.STAR).map(f => f.field).join(',')} from ${table.name}`;
-            return builder(builders, table, sql);
+            const sql = `select ${table.getFields(fields, select.STAR).map(f => f.field).join(', ')} from ${table.name}`;
+            return builder( builders, table, sql );
         }
     };
 }
